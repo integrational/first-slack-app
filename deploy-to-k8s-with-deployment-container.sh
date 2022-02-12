c6943b8a649e8f8d63c20b1dbcfdd04c95d9c9fd
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-02-12 22:39:58 +0100

    presumably fixed bugs in deploy scripts

--- deploy-to-k8s-with-deployment-container.sh
+++ deploy-to-k8s-with-deployment-container.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-set -Eeuo pipefail
-
-docker run --pull always --rm -it                                     \
+docker run --pull always --rm -t                                      \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION \
   -e SLACK_SIGNING_SECRET -e SLACK_BOT_TOKEN -e IMG                   \
   -v /var/run/docker.sock:/var/run/docker.sock                        \
   -v $(pwd):/work -w /work                                            \
-  integrational/eks-client /bin/bash -c                               \
-    'aws eks update-kubeconfig --name gerald-research --region eu-central-1;
-     kubectl cluster-info;
-     ./deploy-to-k8s.sh'
+  integrational/eks-client /bin/bash -c '
+    aws eks update-kubeconfig --name gerald-research --region eu-central-1
+    kubectl cluster-info
+    ./deploy-to-k8s.sh
+  '
