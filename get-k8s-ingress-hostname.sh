2403a37ced8707de49d126ec781f3fdc2e8df5e2
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-02-12 22:33:53 +0100

    trying to debug deploy script

--- get-k8s-ingress-hostname.sh
+++ get-k8s-ingress-hostname.sh
@@ -2,7 +2,13 @@ docker run --pull always --rm -it                                     \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION \
   -v /var/run/docker.sock:/var/run/docker.sock                        \
   -v $(pwd):/work -w /work                                            \
-  integrational/eks-client /bin/bash -c '
-    aws eks update-kubeconfig --name gerald-research --region eu-central-1 > /dev/null
-    kubectl get service/first-slack-app -n first-slack-app -o json | jq ".status.loadBalancer.ingress[0].hostname"
-  '
+  integrational/eks-client /bin/bash -c 'aws eks update-kubeconfig --name gerald-research --region eu-central-1'
+
+# docker run --pull always --rm -it                                     \
+#   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION \
+#   -v /var/run/docker.sock:/var/run/docker.sock                        \
+#   -v $(pwd):/work -w /work                                            \
+#   integrational/eks-client /bin/bash -c '
+#     aws eks update-kubeconfig --name gerald-research --region eu-central-1 > /dev/null
+#     kubectl get service/first-slack-app -n first-slack-app -o json | jq ".status.loadBalancer.ingress[0].hostname"
+#   '
