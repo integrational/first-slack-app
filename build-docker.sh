4c77bd923689efdd757156d3578ec77104086c90
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-02-13 15:10:57 +0100

    GitHub Secret ENV to be eval-ed must not contain comment lines

--- build-docker.sh
+++ build-docker.sh
@@ -2,8 +2,5 @@
 set -Eeuo pipefail
 cd $(cd "$(dirname "$0")" && pwd) # cd to where this script is located
 
-echo img1 $IMG
 ./build-node.sh
-echo img4 $IMG
 docker build -t $IMG .
-echo img5 $IMG
--- build-node.sh
+++ build-node.sh
@@ -2,6 +2,4 @@
 set -Eeuo pipefail
 cd $(cd "$(dirname "$0")" && pwd) # cd to where this script is located
 
-echo img2 $IMG
 npm install
-echo img3 $IMG
